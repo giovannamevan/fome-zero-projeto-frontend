01f7f2e3b641db43a62dba3651df0ffb5d9a7439
Giovanna <giovannaevangelista78@gmail.com>
2023-10-18 21:20:28 -0300

    adição de javascript

--- JavaScript/script-login.js
+++ JavaScript/script-login.js
@@ -1,53 +1,25 @@
-// login doador
+function validateForm() {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    var isDoador = document.getElementById('option').checked;
+    var isInteressado = document.getElementById('interessado').checked;
 
-document.addEventListener('DOMContentLoaded', function () {
-    var form = document.querySelector('form');
+    if (!email || !password) {
+        alert('Por favor, preencha todos os campos.');
+        return false;
+    }
 
-    form.addEventListener('submit', function (event) {
-        event.preventDefault();
+    if (!isDoador && !isInteressado) {
+        alert('Por favor, selecione Doador ou ONG/Organização.');
+        return false;
+    }
 
-        var email = document.getElementById('email-doador').value;
-        var password = document.getElementById('password-doador').value;
+    // Adicione a lógica de redirecionamento
+    if (isDoador) {
+        window.location.href = '../Usuário/Doador/perfil-doador.html';
+    } else {
+        window.location.href = '../Usuário/Interessado/perfil-interessado.html';
+    }
 
-        if (email.trim() === '' || password.trim() === '') {
-            alert('Por favor, preencha todos os campos.');
-            return;
-        }
-
-        // Se a validação passar, redirecionará para a página do perfil do doador
-        window.location.href = '.. /Usuário/Doador/perfil-doador.html';
-    });
-});
-
-// login ong
-
-document.addEventListener('DOMContentLoaded', function () {
-    var loginForm = document.getElementById('loginForm');
-    var errorMessage = document.getElementById('errorMessage');
-
-    loginForm.addEventListener('submit', function (event) {
-        event.preventDefault();
-        var email = document.getElementById('email').value;
-        var password = document.getElementById('password').value;
-
-        if (email.trim() === '' || password.trim() === '') {
-            errorMessage.innerText = 'Por favor, preencha todos os campos.';
-            return;
-        }
-        window.location.href = '.. /Usuário/Interessado/perfil-interessado.html';
-    });
-});
-
-// logica de redirecionamento para a pagina de login, se algum produto for escolhido na pagina principal
-document.addEventListener('DOMContentLoaded', function () {
-    // Obtém todos os botões na página
-    var buttons = document.querySelectorAll('button');
-
-    // Adiciona um ouvinte de evento para cada botão
-    buttons.forEach(function (button) {
-        button.addEventListener('click', function () {
-            // Redireciona para a página de login
-            window.location.href = '../Login/login-interessado.html';
-        });
-    });
-});
+    return false; // Evitar o envio padrão do formulário
+}
